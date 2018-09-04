1c2faac390942927f8b81b2c8449effbcf70ae68
Isharabahmed <isharab150@gmail.com>
2018-09-04 15:41:12 +0500

    changed the button of the calculato

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="calculator">
-    <div class="display">{{ current || '0' }}</div>
-    <div @click="clear" class="btn">AC</div>
-    <div @click="sign" class="btn">+/-</div>
-    <div @click="percent" class="btn">%</div>
-    <div @click="divide" class="btn operator">÷</div>
-    <div @click="append('7')" class="btn">7</div>
-    <div @click="append('8')" class="btn">8</div>
-    <div @click="append('9')" class="btn">9</div>
-    <div @click="times" class="btn operator">x</div>
-    <div @click="append('4')" class="btn">4</div>
-    <div @click="append('5')" class="btn">5</div>
-    <div @click="append('6')" class="btn">6</div>
-    <div @click="minus" class="btn operator">-</div>
-    <div @click="append('1')" class="btn">1</div>
-    <div @click="append('2')" class="btn">2</div>
-    <div @click="append('3')" class="btn">3</div>
-    <div @click="add" class="btn operator">+</div>
-    <div @click="append('0')" class="zero btn">0</div>
-    <div @click="dot" class="btn">.</div>
-    <div @click="equal" class="btn operator">=</div>
+    <button class="display">{{ current || '0' }}</button>
+    <button @click="clear" class="btn">C</button>
+    <button @click="sign" class="btn">+/-</button>
+    <button @click="percent" class="btn">%</button>
+    <button @click="buttonide" class="btn operator">÷</button>
+    <button @click="append('7')" class="btn">7</button>
+    <button @click="append('8')" class="btn">8</button>
+    <button @click="append('9')" class="btn">9</button>
+    <button @click="times" class="btn operator">x</button>
+    <button @click="append('4')" class="btn">4</button>
+    <button @click="append('5')" class="btn">5</button>
+    <button @click="append('6')" class="btn">6</button>
+    <button @click="minus" class="btn operator">-</button>
+    <button @click="append('1')" class="btn">1</button>
+    <button @click="append('2')" class="btn">2</button>
+    <button @click="append('3')" class="btn">3</button>
+    <button @click="add" class="btn operator">+</button>
+    <button @click="append('0')" class="zero btn">0</button>
+    <button @click="dot" class="btn">.</button>
+    <button @click="equal" class="btn operator">=</button>
   </div>
 </template>
 
@@ -59,7 +59,7 @@ export default {
       this.previous = this.current;
       this.operatorClicked = true;
     },
-    divide() {
+    buttonide() {
       this.operator = (a, b) => a / b;
       this.setPrevious();
     },
@@ -95,11 +95,14 @@ export default {
   display: grid;
   grid-template-columns: repeat(4 , 1fr);
   grid-auto-row: minmax(50px, auto);
+  radius: 10px;
 }
 .display{
   grid-column: 1/5;
   background-color: black;
   color: white;
+  padding: 16px;
+  font-size: 24px;
 }
 .zero{
   grid-column: 1/3;
@@ -107,6 +110,8 @@ export default {
 .btn{
   background-color: #eee;
   border: 1px solid #999;
+  padding: 15px;
+  font-size: 24px;
 }
 .operator{
   background-color: orange;
